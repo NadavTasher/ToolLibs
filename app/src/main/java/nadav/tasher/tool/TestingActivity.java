0f8b0c1bfad5b208a89cd1944a702c69f9e12265
nadav <tashernadav@gmail.com>
2018-08-22 11:01:36 -0400

    Version: 4.6 Build-Code: 46 Commit Number: 04

--- app/src/main/java/nadav/tasher/tool/TestingActivity.java
+++ app/src/main/java/nadav/tasher/tool/TestingActivity.java
@@ -20,15 +20,13 @@ import nadav.tasher.lightool.graphics.views.Utils;
 import nadav.tasher.lightool.graphics.views.appview.AppView;
 import nadav.tasher.lightool.graphics.views.appview.navigation.corner.Corner;
 import nadav.tasher.lightool.info.Device;
-import nadav.tasher.lightool.parts.Peer;
 
 public class TestingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final AppView view = new AppView(this);
-        new Peer<String>().
-                view.getDrawer().getDrawerView().setBackground(Utils.getCoaster(Color.BLACK, 64, 20));
+        view.getDrawer().getDrawerView().setBackground(Utils.getCoaster(Color.BLACK, 64, 20));
         view.getDrawer().setAnimationTime(1000);
         Corner c = new Corner(getApplicationContext(), Device.screenX(getApplicationContext()) / 5, 0xFF123987);
         ImageView imageView = new ImageView(getApplicationContext());
@@ -80,12 +78,11 @@ public class TestingActivity extends Activity {
         myTextB.setTextSize(32);
         myTextB.setText("This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.\nThis Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.\nThis Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.This Is A Text.\nThis Is A Text.");
         myTextB.measure(ViewGroup.LayoutParams.MATCH_PARENT, View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        Log.i("BHO",""+myTextB.getMeasuredHeight());
-
+        Log.i("BHO", "" + myTextB.getMeasuredHeight());
         //        myTextB.setPadding(0,30,0,30);
-//        myTextB.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Device.screenY(getApplicationContext()) / 13));
-        ExpandingView ev = new ExpandingView(getApplicationContext(), Utils.getCoaster(Color.WHITE, 32, 10),500, Device.screenY(getApplicationContext()) / 13, myTextA, myTextB);
-        LinearLayout ll=new LinearLayout(getApplicationContext());
+        //        myTextB.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Device.screenY(getApplicationContext()) / 13));
+        ExpandingView ev = new ExpandingView(getApplicationContext(), Utils.getCoaster(Color.WHITE, 32, 10), 500, Device.screenY(getApplicationContext()) / 13, myTextA, myTextB);
+        LinearLayout ll = new LinearLayout(getApplicationContext());
         ll.setGravity(Gravity.CENTER);
         ll.setOrientation(LinearLayout.VERTICAL);
         ll.addView(ev);
